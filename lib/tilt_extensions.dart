04cfeee94871c406373bfe28a5672b2fb072f3a5
rkishan516 <rkishan516@gmail.com>
2024-07-22 13:44:43 +0530

    Rename provider extension

--- lib/tilt_extensions.dart
+++ lib/tilt_extensions.dart
@@ -8,5 +8,5 @@ export 'src/global_key_extension.dart';
 export 'src/list_extension.dart';
 export 'src/math_extension.dart';
 export 'src/object_extension.dart';
-export 'src/provider_extesnion.dart';
+export 'src/provider_extension.dart';
 export 'src/string_extension.dart';
